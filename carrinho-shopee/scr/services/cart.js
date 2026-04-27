8e0d994adf47943136fa0c93195a2f1a337cfc24
gabrielyvitoria0 <gabrielyv896@gmail.com>
2026-04-27 11:34:28 -0300

    Update cart.js

--- carrinho-shopee/scr/services/cart.js
+++ carrinho-shopee/scr/services/cart.js
@@ -1,9 +1,9 @@
 //quais ações meu carrinho pode fazer
 
 //caso de uso
-//adicionar item no carrinho
+//✅adicionar item no carrinho
 async function addItem(userCart){
-
+    userCart.push(item);
 }
 
 //remover item do carrinho
@@ -12,8 +12,22 @@ async function removeItem(userCart, index){
 }
 
 //deletar item do carrinho
-
 async function deletItem(userCart, name){
+    const index = userCart.findIndex ((item) => item.name === name );
 
+    if(index !== -1){
+        userCart.splice(index, 1)
+    }
 }
+
 //calcular o total
+async function calculateTotal(userCart){
+    return userCart.reduce((total, item) => total + item.subtotal () 0);
+}
+
+export {
+    addItem,
+    calculateTotal,
+    deletItem,
+    removeItem
+};
