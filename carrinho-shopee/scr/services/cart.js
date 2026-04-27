48af140e7eddcb6425da3d397d64aead886387bc
gabrielyvitoria0 <gabrielyv896@gmail.com>
2026-04-27 08:51:26 -0300

    Add files via upload

---
+++ carrinho-shopee/scr/services/cart.js
@@ -0,0 +1,19 @@
+//quais ações meu carrinho pode fazer
+
+//caso de uso
+//adicionar item no carrinho
+async function addItem(userCart){
+
+}
+
+//remover item do carrinho
+async function removeItem(userCart, index){
+
+}
+
+//deletar item do carrinho
+
+async function deletItem(userCart, name){
+
+}
+//calcular o total
