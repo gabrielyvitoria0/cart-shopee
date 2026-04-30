156daa84a8b1ef4d7085de597d6104a780f1bdbb
gabrielyvitoria0 <gabrielyv896@gmail.com>
2026-04-30 12:48:34 -0300

    Update index.js

--- carrinho-shopee/scr/index.js
+++ carrinho-shopee/scr/index.js
@@ -1,11 +1,28 @@
-import * cartService from './services/item.js'
+import * as cartService from './services/item.js'
 import createItem from "./services/item.js";
 
-const cart = []
+const myCart = [];
+const myWhishList = [];
+
 
 console.log("Welcome to the Shopee Cart!")
 
 const item1 = await createItem("hotwheels ferrari", 20.99, 1);
 const item2 = await createItem("hotwheels lamborghini",39.99, 3);
 
-console.log(item2.subtotal());
+//adicionar 2 itens no carrinho 
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+await cartService.removeItem(myCart, item2);
+await cartService.removeItem(myCart, item2);
+await cartService.removeItem(myCart, item2);
+
+await cartService.displaycart(myCart);
+
+//deletei 2 itens do carrinho 
+await cartService.deleteItem(myCart, item2.name);
+await cartService.deleteItem(myCart, item1.name);
+
+
+await cartService.calculeTotal(myCart);
